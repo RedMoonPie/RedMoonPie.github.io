ed5ae4751b3b8c30cdf22f2c5e611245295234f7
RedMoonPie <die.fer04@hotmail.com>
2020-10-10 20:29:41 -0500

    Algunos fixes,se empezo la pagina 3

--- js/Scroll.js
+++ js/Scroll.js
@@ -1,21 +1,15 @@
-var target = $(".Main_Logo");
-var targetHeight = 600;
-var i = 1;
-var scrollPercent = (targetHeight - window.scrollY) / targetHeight;
-/*
-$(document).scroll(function(e){
-    
-    console.log(window.scrollY)
-    console.log(scrollPercent)
-    if(scrollPercent >= 0){
-        scrollPercent = (targetHeight - window.scrollY) / targetHeight;
-        target.css('opacity', scrollPercent);
+var glide = new Glide('#intro', {
+    type: 'carousel',
+    perView: 4,
+    focusAt: 'center',
+    breakpoints: {
+      800: {
+        perView: 2
+      },
+      480: {
+        perView: 1
+      }
     }
-    else {
-        i = i+1 
-        scrollPercent = 1;
-        console.log(`Menor ${scrollPercent}`)
-        targetHeight = targetHeight * i
-    }
-});
-*/
+  })
+  
+  glide.mount()
