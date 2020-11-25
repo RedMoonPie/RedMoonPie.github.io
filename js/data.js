c8257ab13f181219e6ba48659ff4e2cc06d1c44d
JohaB-V <johanabautista0@gmail.com>
2020-11-24 21:14:45 -0500

    Minor fixes

--- js/data.js
+++ js/data.js
@@ -1,9 +1,10 @@
 var certifications = {
-    development: [
+  development: [
     {
-      id: 1, 
+      id: 1,
       title: "Front-end Developer",
-      cover: "./assets/certificates/Development/diploma-frontend-developer-1.jpg",
+      cover:
+        "./assets/certificates/Development/diploma-frontend-developer-1.jpg",
     },
     {
       id: 2,
@@ -13,7 +14,8 @@ var certifications = {
     {
       id: 3,
       title: "Profesional de Js",
-      cover: "./assets/certificates/Development/diploma-javascript-profesional-1.jpg",
+      cover:
+        "./assets/certificates/Development/diploma-javascript-profesional-1.jpg",
     },
     {
       id: 4,
@@ -28,7 +30,8 @@ var certifications = {
     {
       id: 6,
       title: "Fundamentos de JS",
-      cover: "./assets/certificates/Development/diploma-fundamentos-javascript-1.jpg",
+      cover:
+        "./assets/certificates/Development/diploma-fundamentos-javascript-1.jpg",
     },
     {
       id: 7,
@@ -43,14 +46,15 @@ var certifications = {
     {
       id: 9,
       title: "Basico de JS",
-      cover: "./assets/certificates/Development/diploma-basico-javascript-1.jpg",
+      cover:
+        "./assets/certificates/Development/diploma-basico-javascript-1.jpg",
     },
 
     {
       id: 10,
       title: "Asincronismo con JS",
       cover: "./assets/certificates/Development/diploma-asincronismo-js-1.jpg",
-    }
+    },
   ],
   management: [
     {
@@ -76,15 +80,16 @@ var certifications = {
     {
       id: 5,
       title: "Técnicas de Negociación",
-      cover: "./assets/certificates/Management/diploma-tecnicas-negociacion-1.jpg",
-    }
+      cover:
+        "./assets/certificates/Management/diploma-tecnicas-negociacion-1.jpg",
+    },
   ],
   design: [
     {
       id: 1,
       title: "Design Sprint",
       cover: "./assets/certificates/Design/diploma-design-sprint-1.jpg",
-    }
+    },
   ],
   personal: [
     {
@@ -96,6 +101,39 @@ var certifications = {
       id: 6,
       title: "Liderazgo para equipos de trabajo",
       cover: "./assets/certificates/Personal/diploma-motivacion-1.jpg",
+    },
+  ],
+};
+var projects = {
+  mylist: [
+    {
+      id: 1,
+      title: "Dijikstra Python",
+      cover: "./assets/Projects/Python/dijikstra",
+      gitlink: "https://github.com/RedMoonPie/djkstra_python.png",
+      description:
+        "Algoritmo de la ruta mas corta, graficación de la ruta y marcado de la ruta",
+      type: "python",
+    },
+    {
+      id: 2,
+      title: "Pilas-Colas-arboles Python",
+      cover: "./assets/Projects/Python/pilas-arboles",
+      gitlink: "https://github.com/RedMoonPie/djkstra_python.png",
+      description:
+        "Programacion de estructuras de datos para el entendimiento de los nodos y su funcionamiento conjunto",
+        type: "python",
+    },
+    {
+      id: 3,
+      title: "Crud de administracion Web Asp.net",
+      cover: "./assets/Projects/Python/pilas-arboles",
+      gitlink: "https://github.com/RedMoonPie/djkstra_python.png",
+      description:
+        "Programacion de estructuras de datos para el entendimiento de los nodos y su funcionamiento conjunto",
+        type: "python",
     }
+
+
   ],
 };
