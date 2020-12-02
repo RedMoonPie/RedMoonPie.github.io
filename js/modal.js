27025b0b6913d444940ead32d7441c8964812e6e
JohaB-V <johanabautista0@gmail.com>
2020-12-01 19:35:01 -0500

    Photo edition - slider fixes

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,5 @@
 var modal = document.getElementById("myModal");
 var myCertifications = [];
-var ele_myCertifications = [];
 var content_image = document.getElementById("content_img");
 
 function Ready() {
@@ -16,10 +15,7 @@ function Ready() {
   });
 }
 
-// Get the <span> element that closes the modal
 
-
-// When the user clicks on <span> (x), close the modal
 modal.onclick = function () {
   modal.style.display = "none";
 };
