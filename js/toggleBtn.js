226226f5608bed5b81da32a5fcc30220a49d7c21
RedMoonPie <die.fer04@hotmail.com>
2020-12-10 22:43:30 -0500

    added 2 certifications

--- js/toggleBtn.js
+++ js/toggleBtn.js
@@ -4,7 +4,6 @@ const button =document.getElementById("toggleButton");
 button.addEventListener("click",toggleButton);
 
 function toggleButton() {
-    console.log("jolasd")
     var linked = document.querySelector(".linked");
     linked.classList.toggle("hideBtn")
 }
